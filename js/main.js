2b18a79fccd6d8e98a2020bf73bbbdf7b4f2e690
shtorm-fakel <ibrohimtalatov2@gmail.com>
2022-02-22 20:22:16 +0300

    initial commit

--- js/main.js
+++ js/main.js
@@ -1,17 +1,23 @@
-var a = Number(500)
+var toTicket = Number(500)
 //samiyotta borib kelish puli $500
-var b = Number(250) 
+var toHotel = Number(250) 
 //mehmonhona to'lovi $250
-var y = Number(120)
+var toWollking = Number(120)
 //muzey va ko'ngilochar joylar uchun 120 yevro
-a=a*10.900;
-b=b*10.900;
-y=y*12272,64;
-var add=Number((a+b+y))
+toTicket = toTicket*10900;
+toHotel = toHotel*10900;
+toWollking = toWollking*12272,64;
+var add=Number((toTicket+toHotel+toWollking))
 console.log(add)
 var summed = prompt("pulni kriting")
 if(summed>=add){
     console.log("alisher pulingiz yetadi gazini bosing");
-}else{
+}
+else if (summed <= "  " ) {
+    console.log("alisher kimni aldavossiz bu son emasku");
+}else if (summed <= 0 ) {
+    console.log("alisher minusdasiz ");
+}
+else{
     console.log("brat puliz yetmadi keyingi safar");
 }
